774ccdcfea23082b0e89d41cb3e6e30a12c448b9
delbertina <6349928+delbertina@users.noreply.github.com>
2024-01-28 17:15:38 -0600

    Initial commit to prepare for pages pipeline

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Fish are Friends",
+  description: "Fish pet simulator inspired by classic 90's web browsing and tamagatchi.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
