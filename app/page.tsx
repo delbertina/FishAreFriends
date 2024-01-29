af37cf8088ca38e017df832831a593604c8d4914
delbertina <6349928+delbertina@users.noreply.github.com>
2024-01-29 00:00:37 -0600

    Basic UI with dynamically arranged buttons

--- app/page.tsx
+++ app/page.tsx
@@ -2,93 +2,54 @@ import Image from "next/image";
 import styles from "./page.module.scss";
 
 export default function Home() {
+  const imgAssetBasePath = "/FishAreFriends/assets/imgs/";
+  const buttonCircleRadius = 200;
+
+  const fishDefaultBasic = "u1f41f_u1f642.png";
+
+  let currentFish = fishDefaultBasic;
+  let actionButtons = ["Pet", "Feed", "Clean", "Play", "Hit", "Medicine", "Joke", "Lights", "Dress"];
+
+  const get_center_img_path = () => {
+    return imgAssetBasePath + currentFish
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/FishAreFriends/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
+      <div
+        className={styles.action_button_container}
+        style={
+          {
+            "--action-button-container-padding": buttonCircleRadius + "px",
+          } as React.CSSProperties
+        }
+      >
         <Image
-          className={styles.logo}
-          src="/FishAreFriends/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          src={get_center_img_path()}
+          alt="Fish emoji normal face"
+          height={100}
+          width={100}
+        ></Image>
+        {actionButtons.map((action, i) => (
+          <button
+            key={i}
+            className={styles.radial_action_button}
+            style={
+              {
+                "--radial-action-button-y":
+                  buttonCircleRadius *
+                    Math.sin((2 * Math.PI * i) / actionButtons.length) +
+                  "px",
+                "--radial-action-button-x":
+                  0-(buttonCircleRadius *
+                    Math.cos((2 * Math.PI * i) / actionButtons.length)) +
+                  "px",
+              } as React.CSSProperties
+            }
+          >
+            {action}
+          </button>
+        ))}
       </div>
     </main>
   );
