69de5db6f4c76922c61266a20b73512b7d1123eb
delbertina <6349928+delbertina@users.noreply.github.com>
2024-01-30 21:31:54 -0600

    Test out animations on button press system. Need to fix some styling still

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,34 @@
+'use client';
+
 import Image from "next/image";
 import styles from "./page.module.scss";
+import { useState } from "react";
 
 export default function Home() {
   const imgAssetBasePath = "/FishAreFriends/assets/imgs/";
+  const fishBounceAnimateClass = styles.fade;
   const buttonCircleRadius = 200;
 
   const fishDefaultBasic = "u1f41f_u1f642.png";
+  const fishActionHit = "u1f41f_u1f4ab.png";
 
-  let currentFish = fishDefaultBasic;
+  const [currentFish, setCurrentFish] = useState(fishDefaultBasic);
   let actionButtons = ["Pet", "Feed", "Clean", "Play", "Hit", "Medicine", "Joke", "Lights", "Dress"];
+
+  const [currentFishClass, setCurrentFishClass] = useState<string>("")
+
+  const delayedChangeToDefault = (millis: number) => {
+    setTimeout(() => {
+      setCurrentFish(fishDefaultBasic);
+      setCurrentFishClass(fishBounceAnimateClass);
+    }, millis);
+  }
+
+  const changeCurrentFish = (newSource: string) => {
+    setCurrentFish(newSource);
+    setCurrentFishClass(fishBounceAnimateClass);
+    delayedChangeToDefault(5000);
+  }
 
   const get_center_img_path = () => {
     return imgAssetBasePath + currentFish
@@ -27,13 +47,17 @@ export default function Home() {
         <Image
           src={get_center_img_path()}
           alt="Fish emoji normal face"
+          className={currentFishClass}
+          onAnimationEnd={() => setCurrentFishClass("")}
           height={100}
           width={100}
+          priority
         ></Image>
         {actionButtons.map((action, i) => (
           <button
             key={i}
             className={styles.radial_action_button}
+            onClick={() => changeCurrentFish(fishActionHit)}
             style={
               {
                 "--radial-action-button-y":
